dc9b54f5ae8cafab81067d75d5ab1b43e675cafb
Ryan Durso <rdurso86@gmail.com>
2020-01-23 20:26:38 -0500

    added questions and answers in javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+let timer = 0;
+
+let questions = [
+    {
+    question: "1. Which is the best vegtable?",
+    choices: ["Celery", "Potato", "Carrot", "Spinach"],
+    correctAnswer: 1
+}, 
+{
+    question: "2. What was the first vegtable to be grown in space?",
+    choices: ["Spinach", "Parsnip", "Potato", "Lettuce"],
+    correctAnswer: 2
+}, 
+{
+    question: "3. Where does the potato originate from?",
+    choices: ["North America", "South America", "Europe", "Asia"],
+    correctAnswer: 1
+}, 
+{
+    question: "4. How many US States grow potatoes?",
+    choices: ["15", "23", "37", "50"],
+    correctAnswer: 3
+}, 
+{
+    question: "5. How many potatoes are used to make a bag of Lay's chips?",
+    choices: ["1 to 2", "4 to 5", "7 to 8", "10 to 11"],
+    correctAnswer: 1
+},
+];
