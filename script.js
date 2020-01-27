1f2e0c83cfd598f2c71fa1e2dea271b7ae1c2e72
Ryan Durso <rdurso86@gmail.com>
2020-01-26 21:49:20 -0500

    simplified html and java

--- script.js
+++ script.js
@@ -1,29 +1,114 @@
-let timer = 0;
+$(document).ready(function() {
+
+const startPage = $(".startPage");
+const startPageButtons = $(".startPageButtons");
+const startQuizBtn = $("#startQuiz");
+const viewHighscoresBtn = $("#viewHighscores");
+
+const questionsPage = $(".questionsPage");
+const timeRemaining = $(".timeRemaining");
+const questionsContainer = $(".questionsContainer");
+const question = $("#question");
+const answerButtons = $("#answerButtons");
+const choice = $(".choice");
+
+const completePage = $(".completePage");
+const score = $(".score");
+const enterInitials = $(".enterInitials");
+
+const highscoresPage = $(".highscoresPage");
+const highscoreList = $(".highscoreList");
+const playAgainBtn = $("#playAgain");
+
+
 
 let questions = [
     {
     question: "1. Which is the best vegtable?",
     choices: ["Celery", "Potato", "Carrot", "Spinach"],
-    correctAnswer: 1
+    answer: 1
 }, 
 {
     question: "2. What was the first vegtable to be grown in space?",
     choices: ["Spinach", "Parsnip", "Potato", "Lettuce"],
-    correctAnswer: 2
+    answer: 2
 }, 
 {
     question: "3. Where does the potato originate from?",
     choices: ["North America", "South America", "Europe", "Asia"],
-    correctAnswer: 1
+    answer: 1
 }, 
 {
     question: "4. How many US States grow potatoes?",
     choices: ["15", "23", "37", "50"],
-    correctAnswer: 3
+    answer: 3
 }, 
 {
     question: "5. How many potatoes are used to make a bag of Lay's chips?",
-    choices: ["1 to 2", "4 to 5", "7 to 8", "10 to 11"],
-    correctAnswer: 1
+    choices: ["4 to 5", "7 to 8", "10 to 11", "12 to 13"],
+    answer: 0
 },
 ];
+
+
+function initializeQuiz() {
+    $(questionsPage).hide();
+    $(completePage).hide();
+    $(highscoresPage).hide();
+};
+
+initializeQuiz();
+
+$(viewHighscoresBtn).on("click", function(){
+    $(startPage).hide();
+    $(questionsPage).hide();
+    $(completePage).hide();
+    $(highscoresPage).show();
+});
+
+$(startQuizBtn).on("click", function(){
+    $(startPage).hide();
+    $(completePage).hide();
+    $(highscoresPage).hide();
+    $(questionsPage).show();
+
+    var i = 60;
+(function timer(){
+    if (--i < 0) return;
+    setTimeout(function(){
+        $(timeRemaining).text("Time Remaining: " + i);
+        timer();
+    }, 1000);
+})();
+
+});
+
+for (let q = 0; q < questions.length; q++) {
+    //let question = questions.question;
+console.log(questions.question);
+};
+
+//why wont this work?
+$(playAgainBtn).on("click", function() {
+    initializeQuiz();
+});
+
+console.log(this);
+
+
+
+});
+// function showQuestion(question) {
+    //     $(question) = questions.question
+    //     questions.choices.forEach(choices => {
+        //       const button = document.createElement('button')
+        //       button.innerText = choices.text
+        //       button.classList.add('btn')
+        //       if (answer.correct) {
+            //         button.dataset.correct = choices.correct
+            //       }
+            //       button.addEventListener('click', selectAnswer)
+            //       answerButtonsElement.appendChild(button)
+            //     })
+            //   }
+            // console.log(showQuestion());
